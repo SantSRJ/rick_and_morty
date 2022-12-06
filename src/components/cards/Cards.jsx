524222f83020a972491dab4ac4f0993f4f14f6f3
Santiago Mendoza <srjmendoza@hotmail.com>
2022-12-06 19:59:04 -0300

    06/12

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,19 +1,19 @@
 import Card from '../card/Card';
 
 export default function Cards(props) {
-   const { characters } = props;
-   return <div>
-      {
-         characters.map(char => (
+   const { characters, onClose } = props;
+   return (
+      <div>
+      {characters.map(char => (
          <Card 
             key={char.name}
             name={char.name}
             species={char.species}
             gender={char.gender}
             image={char.image}
-            onClose={() => alert("Emulamos que se cierra la card")}
+            onClose={() => onClose(char.id)}
          />
-         ))
-      }
-         </div>;
+         ))}
+      </div>
+   );
 }
--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,9 +1,17 @@
+import { useState } from 'react'
+
 export default function SearchBar(props) {
    const {onSearch} = props;
+   const [character, setCharacter] = useState("");
+
+   const handleChange = (e) => {
+      setCharacter(e.target.value);
+   }
+
    return (
       <div>
-         <input type='search' />
-      <button onClick={()=> onSearch("Futuro ID del personaje")}>Agregar</button>
+         <input type='search' value={character} onChange={handleChange}/>
+         <button onClick={() => onSearch(character)}>Agregar</button>
       </div>
    );
 }
