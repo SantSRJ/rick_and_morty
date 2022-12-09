fafccc2ec541dadf504718a6ac96dd4424f3448f
Santiago Mendoza <srjmendoza@hotmail.com>
2022-12-09 11:00:28 -0300

    09/12

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -4,7 +4,14 @@ export default function About() {
   return (
     <div>
         <h1>Mi primera API</h1>
-        <h5>Creada por Santiago Mendoza</h5>
+        <h3>Creada por Santiago Mendoza</h3>
+        <p>
+        La API de Rick and Morty es una API REST(ish) y GraphQL basada en el programa de televisión Rick and Morty. Tendrás acceso a cientos de personajes, imágenes, lugares y episodios. La API de Rick and Morty está llena de información canónica como se ve en el programa de televisión.
+        </p>
+        <br/>
+        <p>
+        The Rick and Morty API is a REST(ish) and GraphQL API based on the television show Rick and Morty. You will have access to about hundreds of characters, images, locations and episodes. The Rick and Morty API is filled with canonical information as seen on the TV show.
+        </p>
     </div>
   )
 }
